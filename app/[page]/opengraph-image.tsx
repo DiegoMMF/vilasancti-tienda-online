4b7d6e5a08c7e5538975aca53c00fb009721a31f
Diego M. Maldini Freyre <diegomaldinifreyre@gmail.com>
2025-08-08 08:13:12 -0300

    Mejora la obtención del título en el componente de imagen OpenGraph, asegurando que se utilicen valores alternativos en caso de que no estén disponibles. Esto incluye el uso de `params.page` como último recurso.

--- app/[page]/opengraph-image.tsx
+++ app/[page]/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { getPage } from 'lib/api/pages';
 
 export default async function Image({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
-  const title = page.seo?.title || page.title;
+  const title = page?.seo?.title || page?.title || params.page;
 
   return await OpengraphImage({ title });
 }
