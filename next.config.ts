b46fea178f1249ca50c802b71f22dd6601129731
DiegoMMF <diegomaldinifreyre@gmail.com>
2025-08-15 15:31:43 -0300

    feat: implement security headers in Next.js configuration
    - Added a set of security headers to enhance application security, including X-DNS-Prefetch-Control, Strict-Transport-Security, X-XSS-Protection, X-Frame-Options, X-Content-Type-Options, and Referrer-Policy.
    - Updated the Next.js configuration to include an async headers function that applies these security headers to all routes.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,30 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 export default {
   experimental: {
     inlineCss: true,
@@ -9,6 +36,14 @@ export default {
         source: "/search/:collection",
         destination: "/category/:collection",
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
       },
     ];
   },
@@ -26,5 +61,7 @@ export default {
         pathname: "/**",
       },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
